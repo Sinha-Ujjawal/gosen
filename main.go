e512e926d91d2cf9034bcaa6bf67ed0b744a4ea0
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2024-01-18 12:29:14 +0530

    Create separate part of code for-
    1. Indexing and Saving to DB
    2. Loading from DB

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	// dirPath := "./docs.gl"
-	dbName := "docs.gl.db"
-	// fileContents, errs := FileContentsFromDirectory(dirPath)
-	// for _, err := range errs {
+	{
+		dirPath := "./docs.gl"
+		dbName := "docs.gl.db"
+		fileContents, errs := FileContentsFromDirectory(dirPath)
+		for _, err := range errs {
+			if err != nil {
+				log.Fatalf("%s", err)
+			}
+		}
+		termFrequenciesIndex := MkTermFrequenciesIndexFromFileContents(fileContents)
+		err := termFrequenciesIndex.DumpToSQLite3(dbName)
+		if err != nil {
+			log.Fatalf("%s", err)
+		}
+	}
+	// {
+	// 	dbName := "docs.gl.db"
+	// 	termFrequenciesIndex, err := LoadTermFrequenciesIndexFromSQLite3(dbName)
 	// 	if err != nil {
 	// 		log.Fatalf("%s", err)
 	// 	}
+	// 	for filePath, tf := range termFrequenciesIndex.tfs {
+	// 		fmt.Printf("File %s has %d terms\n", filePath, len(tf))
+	// 	}
 	// }
-	// termFrequenciesIndex := MkTermFrequenciesIndexFromFileContents(fileContents)
-	// // for filePath, tf := range termFrequenciesIndex.tfs {
-	// // 	fmt.Printf("File %s has %d terms\n", filePath, len(tf))
-	// // }
-	// err := termFrequenciesIndex.DumpToSQLite3(dbName)
-	// if err != nil {
-	// 	log.Fatalf("%s", err)
-	// }
-	termFrequenciesIndex, err := LoadTermFrequenciesIndexFromSQLite3(dbName)
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-	for filePath, tf := range termFrequenciesIndex.tfs {
-		fmt.Printf("File %s has %d terms\n", filePath, len(tf))
-	}
-	_ = termFrequenciesIndex
 }
