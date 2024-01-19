0766979b978c2bc5dc302a02a57f28cc526ea092
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2024-01-19 18:52:22 +0530

    Making sure to trim and convert to uppercase in tokenize

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func usage(program string) {
 }
 
 func tokenize(text string) []string {
+	text = strings.ToUpper(strings.TrimSpace(text))
 	return tokenizer.SimpleTokenizerFromString(text).Tokens()
 }
 
