570801d745c5deb99c1d04d7c523b8e49a22c1e1
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2024-01-21 22:05:39 +0530

    JSDoc index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @param {string} header - header text
+ * @returns HTMLSpanElement - header as a span element
+ */
 function mkHeader(header) {
     let item = document.createElement("span");
     let boldHeader = document.createElement("strong");
@@ -7,6 +12,11 @@ function mkHeader(header) {
     return item;
 }
 
+/**
+ * 
+ * @param {string} prompt - query string
+ * @param {integer} topN - top n results to show
+ */
 async function search(prompt, topN) {
     if (topN === undefined || isNaN(topN) || topN <= 0) {
         topN = 1
@@ -16,18 +26,24 @@ async function search(prompt, topN) {
         "topN": topN,
     }
     const results = document.getElementById("results")
+    if (results == null) {
+        return
+    }
     results.innerHTML = "";
     const response = await fetch("/api/search", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(query),
     });
+    /**
+     * @type {[{docId: string, score: number}]}
+     */
     const jsonArr = await response.json();
     results.innerHTML = "";
     let headerNode = mkHeader(`Showing top ${Math.min(topN, jsonArr.length)} results:`);
     results.appendChild(headerNode);
     for (let i = 0; i < jsonArr.length; i++) {
-        let { docId, _score } = jsonArr[i];
+        let { docId, score } = jsonArr[i];
         let item = document.createElement("span");
         item.appendChild(document.createTextNode(docId));
         item.appendChild(document.createElement("br"));
@@ -40,6 +56,8 @@ let topN = document.getElementById("topN");
 let searchButton = document.getElementById("search");
 let currentSearch = Promise.resolve()
 
-searchButton.onclick = function (e) {
-    currentSearch.then(() => search(query.value, topN.value * 1));
+if (searchButton !== null) {
+    searchButton.onclick = function (e) {
+        currentSearch.then(() => search(query.value, topN.value * 1));
+    }
 }
